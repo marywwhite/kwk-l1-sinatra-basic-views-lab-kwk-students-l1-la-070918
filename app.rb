a10e15e48c26f5b9fb958b7b3997caa506c51aff
Mary White <maryewhite2022@gmail.com>
2018-07-20 16:23:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,33 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 get '/' do 
-  erb :index
+  erb :home
 end
+
+get '/fashion' do
+  erb :fashion
+end
+
+get '/beauty' do
+  erb :beauty 
+end
+
+get '/articles' do
+  erb :articles
+end
+
+get '/aboutus' do
+  erb :aboutus
+end
+
+get '/contact' do
+  erb :aboutus
+end
+
+get '/subscribe' do
+  erb :aboutus
+end
+
+
 
 end
